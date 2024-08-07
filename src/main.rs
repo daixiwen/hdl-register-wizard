2757f124531c667a33f2083978375d16ad7983db
Sylvain Tertois <sylvain-gith@becoz.org>
2024-08-07 14:30:49 +0200

    added icon and install script for linux

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,35 @@ use dioxus::prelude::*;
 /// When compiling natively, open a window and launch the application
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
+    use hdl_register_wizard::assets;
+
     let app_settings = app::HdlWizardApp::try_load();
     let window_pos = app_settings.data.target.window_pos.borrow();
     let window_size = app_settings.data.target.window_size.borrow();
 
+    let icon = match assets::find_asset("icon.png") {
+        None => {
+            println!("icon file not found!");
+            None},
+        Some(icon_path) => {
+            match image::open(icon_path) {
+                Ok(img) => {
+                    match tao::window::Icon::from_rgba(img.to_rgba8().as_raw().to_owned(), 512, 512) {
+                        Ok(data) => Some(data),
+                        Err(e) => {
+                            println!("Error converting icon: {e}");
+                            None
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("error while loading icon: {e}");
+                    None
+                }
+            }
+        }
+    };
+    
     dioxus_desktop::launch::launch(
         app::App,
         Vec::new(),
@@ -25,7 +50,8 @@ fn main() {
                 .with_inner_size(window_size.to_owned())
                 .with_position(window_pos.to_owned())
                 .with_title("HDL Register Wizard")
-            ).with_menu(None),
+            ).with_menu(None)
+             .with_icon(icon.expect("no icon to load")),
     );
 }
 
