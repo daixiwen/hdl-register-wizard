507a88962d7f7ee6af6d9f6fd0bf3af6d2b16b4e
Sylvain Tertois <sylvain-gith@becoz.org>
2024-08-19 22:16:31 +0200

    fixed the documentation template when working with fields. fixes #7

--- src/generate/documentation.rs
+++ src/generate/documentation.rs
@@ -16,9 +16,8 @@ fn map_tera_error(tera_error: tera::Error) -> String {
 pub fn generate_doc(model: &genmodel::GenModel, templates: &Tera) -> Result<String, Box<dyn Error>> {
 
 //    let markdown = templates.render("documentation.md", &tera::Context::from_serialize(&model)?)?;
-    let markdown = templates.render("documentation.md", &tera::Context::from_serialize(&model)?);
+    let markdown = templates.render("documentation.md", &tera::Context::from_serialize(&model)?).map_err(map_tera_error)?;
 
-    let markdown = markdown.map_err(map_tera_error)?;
     Ok(mini_markdown::render(&markdown))
     //Ok(markdown)
 }
