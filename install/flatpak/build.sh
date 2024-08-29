df2d923e367763ad0403472aca71b4743fe6b1f3
Sylvain Tertois <sylvain-gith@becoz.org>
2024-08-29 23:09:47 +0200

    working flatpak

--- install/flatpak/build.sh
+++ install/flatpak/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir install/flatpak/org.becoz.hdlregisterwizard.yml
-flatpak build-bundle repo hdlregisterwizard.flatpak org.becoz.hdlregisterwizard
+flatpak build-bundle repo hdlregisterwizard-x64-$1.flatpak org.becoz.hdlregisterwizard
