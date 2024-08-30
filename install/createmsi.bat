3482550fad41dc7db317ff6bfebdd762059fd241
Sylvain Tertois <sylvain-gith@becoz.org>
2024-08-30 12:47:57 -0700

    update wix scripts to use branch/tag name in msi file name

--- install/createmsi.bat
+++ install/createmsi.bat
@@ -1,6 +1,6 @@
 @echo creating MSI file using the WiX Toolset...
 
 candle -arch x64 hdlregisterwizard.wxs
-light -ext WixUIExtension -cultures:en-us hdlregisterwizard.wixobj
+light -ext WixUIExtension -cultures:en-us -out hdlregisterwizard-%1.msi hdlregisterwizard.wixobj
 candle hdlregisterwizard-user.wxs
-light -ext WixUIExtension -cultures:en-us hdlregisterwizard-user.wixobj
+light -ext WixUIExtension -cultures:en-us -out hdlregisterwizard-user-%1.msi hdlregisterwizard-user.wixobj
