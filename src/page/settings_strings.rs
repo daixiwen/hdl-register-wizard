d5f1fdbf2adec1ed2d07df4b7cc63ed47adc7b05
Sylvain Tertois <sylvain-gith@becoz.org>
2024-09-08 23:52:16 +0200

    added patterns to check the user strings

--- src/page/settings_strings.rs
+++ src/page/settings_strings.rs
@@ -7,7 +7,7 @@ use crate::generate::user_strings;
 use core::slice::Iter;
 
 // table line with one string
-fn TableLine(mut app_data: Signal<HdlWizardApp>, template: &user_strings::UserStringSpec) -> Element {
+fn TableLine(mut app_data: Signal<HdlWizardApp>, template: &user_strings::UserStringSpec, pattern: &str) -> Element {
     let value = if let Some(setting_value) = app_data.read().data.settings.user_templates.get(template.template_name) {
         setting_value.clone()
     } else { "??".to_owned()};
@@ -28,6 +28,7 @@ fn TableLine(mut app_data: Signal<HdlWizardApp>, template: &user_strings::UserSt
                                 class: "input",
                                 r#type: "text",
                                 placeholder: "{template.default_value}",
+                                pattern: "{pattern}",
                                 size: "40",
 //                                pattern: "{validate_pattern}",
                                 onchange: move |evt| {
@@ -55,7 +56,7 @@ fn TableLine(mut app_data: Signal<HdlWizardApp>, template: &user_strings::UserSt
     }
 }
 
-fn Table(app_data: Signal<HdlWizardApp>, templates: Iter<'_, user_strings::UserStringSpec>) -> Element {
+fn Table(app_data: Signal<HdlWizardApp>, templates: Iter<'_, user_strings::UserStringSpec>, pattern: &str) -> Element {
     rsx! {
         table {
             class:"table is-striped is-hoverable is-fullwidth",
@@ -67,7 +68,7 @@ fn Table(app_data: Signal<HdlWizardApp>, templates: Iter<'_, user_strings::UserS
                 }
             }
             tbody {
-                {templates.map(|template| TableLine(app_data, template))}
+                {templates.map(|template| TableLine(app_data, template, pattern))}
             }
         }
     }
@@ -76,17 +77,22 @@ fn Table(app_data: Signal<HdlWizardApp>, templates: Iter<'_, user_strings::UserS
 // main page
 #[component]
 pub fn Content(app_data: Signal<HdlWizardApp>) -> Element {
+    // this string needs to be made here or else the rsx macro will try to format it, and escaping { and } seems to work
+    // in different ways between the web and desktop platforms
+    let names_description = r#"Each string must have a * for digits to prevent duplicates, and can also use "{{ project }}", "{{ interface }}", "{{ register }}" and "{{ field }}", when applicable. The resulting string must be a valid VHDL identifier."#;
+    let description_description = r#"These strings are used in comments or the documentation. They can contain "{{ full_name }}", which will be replaced by the name of the object described."#;
+
     rsx! {
         h1 { class: "title page-title", "Settings: Strings" },
         p { "Configure here how the different strings in the generated code and documentation are generated."}
         h1 { class: "subtitle page-title", "Names" },
-        p { "Each string must have a * for digits to prevent duplicates, and can also use \"{{{{ project }}}}\", \"{{{{ interface }}}}\", \"{{{{ register }}}}\" and \"{{{{ field }}}}\", when applicable. The resulting string must be a valid VHDL identifier."}
+        p { {names_description } }
 
-        { Table(app_data, user_strings::USER_NAMES_SPECS.iter()) }
+        { Table(app_data, user_strings::USER_NAMES_SPECS.iter(),r"^(\{\{ *(project|interface|register|field) *\}\}|[A-Za-z])(\{\{ *(project|interface|register|field) *\}\}|[0-9A-Za-z_])*\*(\{\{ *(project|interface|register|field) *\}\}|[0-9A-Za-z_])*$") }
 
         h1 { class: "subtitle page-title", "Descriptions" },
-        p { "These strings are used in comments or the documentation. They can contain \"{{{{ full_name }}}}\", which will be replaced by the name of the object described."}
+        p { {description_description} }
 
-        { Table(app_data, user_strings::USER_COMMENTS_SPECS.iter()) }
+        { Table(app_data, user_strings::USER_COMMENTS_SPECS.iter(), r"^(\{\{ *full_name *\}\}|[^\{\}])*$") }
     }
 }
