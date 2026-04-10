aa3f218ec7abe94990107afe1d4f32ce9a1a2e99
Sylvain Tertois <sylvain-gith@becoz.org>
2026-04-11 00:37:09 +0200

    fix some formatting problems in the pkg.vhd template

--- src/templates/pkg.vhd
+++ src/templates/pkg.vhd
@@ -12,14 +12,17 @@ package {{ pkg_name }} is
   -- Register list
   type {{ interface.register_enum_name }} is
 
-  {%- for register in interface.registers -%}
-    {{ register.token_name}}{%- if (loop.index != loop.last) -%},{%- endif %}
-  {% endfor %};
+  {%- for register in interface.registers %}
+    {{ register.token_name}}
+      {%- if not loop.last -%} 
+        ,
+      {%- endif -%}
+  {%- endfor %};
 
   -- Addresses list
   {% for register in interface.registers -%}
   constant {{ register.address_const_name }} : integer := 16#{{ register.address_hex}}#;
-  {% endfor %};
+  {% endfor -%}
 
   {%- if interface.use_stride %}
   -- Stride constants list
@@ -36,33 +39,32 @@ package {{ pkg_name }} is
       type {{ register.stride_array_type }} is array ({{ register.stride_count - 1 }} downto 0) of {{ register.fields.0.sig_type_complete }};
       {%- endif -%}
     {%- endfor -%}
-  {%- endif -%}
-
+  {%- endif %}
   -- Address decoding functions
   function {{ interface.address_decoder_name }} (address : unsigned) return {{ interface.register_enum_name }};
   {%- if interface.use_stride %}
   function {{ interface.address_stride_func_name }} (address : unsigned) return integer;
-  {% endif -%}
+  {% endif %}
 
   -- records between core and PIF
   type {{ interface.core2pif_name }} is record
-  {% for register in interface.registers -%}
-    {% for field in register.fields -%}
-      {% for entry in field.core2pif -%}
+  {%- for register in interface.registers -%}
+    {%- for field in register.fields -%}
+      {% for entry in field.core2pif %}
     {{ entry.name }} : {{ entry.signal_type }};  -- {{ entry.description }}
       {%- endfor -%}    
     {%- endfor -%}    
-  {%- endfor -%}    
+  {%- endfor %}    
   end record  {{ interface.core2pif_name }};
 
   type {{ interface.pif2core_name }} is record
-  {% for register in interface.registers -%}
-    {% for field in register.fields -%}
-      {% for entry in field.pif2core -%}
+  {%- for register in interface.registers -%}
+    {%- for field in register.fields -%}
+      {% for entry in field.pif2core %}
     {{ entry.name }} : {{ entry.signal_type }};  -- {{ entry.description }}
       {%- endfor -%}    
     {%- endfor -%}    
-  {%- endfor -%}    
+  {%- endfor %}    
   end record  {{ interface.pif2core_name }};
 
 {%- endmacro pkg_interface -%}
@@ -81,6 +83,6 @@ package {{ pkg_name }} is
 {{ self::pkg_interface(interface = interface) }}
 {% endfor %}
 
-{%- endif -%}
+{%- endif %}
 
 end package {{ pkg_name }};
